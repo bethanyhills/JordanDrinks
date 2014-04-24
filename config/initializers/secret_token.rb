d06e59fa151eb22d22a4df57cf1a093a4baeb03b
Bethany Nagel <beth0851@gmail.com>
2014-04-24 15:32:24 -0500

    Add basic blog functionality with basic authentication.  Add HTML and CSS to make site responsive

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'df0f6de0ed28c517863c17725fa5f1593058ba586731fbf05e17ffeca531866c984bffcaf10bb17b050b48b2b6706a65dff00dc0d9846854692ef2a94153476c'
